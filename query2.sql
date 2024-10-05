10486fc6e4dae2fc51adb10206fff7ad002432b2
alexmude <muderhwa.a@northeastern.edu>
2024-10-04 20:49:08 -0700

    answers files to queries 1 - 7.

---
+++ query2.sql
@@ -0,0 +1,12 @@
+/*Queries that insert at least 10 videos, respecting the problem rules. */
+
+INSERT INTO MusicVideo (TrackId, VideoDirector) VALUES (1, 'Russel Mulcahy');
+INSERT INTO MusicVideo (TrackId, VideoDirector) VALUES (2, 'Dieter Rubach');
+INSERT INTO MusicVideo (TrackId, VideoDirector) VALUES (3, 'Accept');
+INSERT INTO MusicVideo (TrackId, VideoDirector) VALUES (4, 'David Mallet');
+INSERT INTO MusicVideo (TrackId, VideoDirector) VALUES (5, 'Accept');
+INSERT INTO MusicVideo (TrackId, VideoDirector) VALUES (6, 'Eric Dionyisus');
+INSERT INTO MusicVideo (TrackId, VideoDirector) VALUES (7, 'Dereck Burbidge');
+INSERT INTO MusicVideo (TrackId, VideoDirector) VALUES (10, 'Eric Dionyisus');
+INSERT INTO MusicVideo (TrackId, VideoDirector) VALUES (12, 'Marc Klasfeld');
+INSERT INTO MusicVideo (TrackId, VideoDirector) VALUES (13, 'Lance Miccio');
