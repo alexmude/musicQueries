24b57ae6473256ac15630f4b08d7fd31485462d9
alexmude <muderhwa.a@northeastern.edu>
2024-10-07 09:27:33 -0700

    comments added

--- query1.sql
+++ query1.sql
@@ -1,10 +1,10 @@
 /*Create a new Table Music Video, that is a class of type Track (generalization)
 but adds the attribute Video director. A music video is a track. 
-There cannot be a video without a track, and each track can have either no video or just one. */
+There cannot be a video without a track, and each track can have either no video or just one.*/
 
 CREATE TABLE MusicVideo (
-    VideoId INTEGER PRIMARY KEY,
-    TrackId INTEGER,
+    VideoId INTEGER PRIMARY KEY NOT NULL,
+    TrackId INTEGER NOT NULL,
     VideoDirector NVARCHAR(120),
     FOREIGN KEY (TrackId) REFERENCES tracks(TrackId) ON DELETE CASCADE
 );
